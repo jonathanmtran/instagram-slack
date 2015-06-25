ff993a8ffed63820f71cf69ef89f039c6d8d7497
Jonathan Tran <jonathanmtran@about.me>
2015-06-24 21:51:14 -0700

    Update to hit the incoming webhook once instead of one for each new post

--- get-recent-media.rb
+++ get-recent-media.rb
@@ -36,6 +36,8 @@ client = Instagram.client(:access_token => access_token)
 
 recent_media = client.user_recent_media(user, :min_timestamp => min_timestamp)
 
+attachments = Array.new
+
 for media_item in recent_media.reverse
 	caption = ''
 	created_time = media_item.created_time
@@ -44,27 +46,34 @@ for media_item in recent_media.reverse
 		caption = media_item.caption.text
 	end
 
-	payload = {
-		:text => 'New Instagram post from *@' + client.user(user).username.to_s + '*',
-
-		:attachments => [
-			{
-				:fallback => caption + ': ' + media_item.link.strip,
-				:image_url => media_item.images.standard_resolution.url,
-				:title => caption,
-				:thumb_url => media_item.images.thumbnail.url,
-				:title_link => media_item.link,
-			},
-		],
+	attachment = {
+		:fallback => caption + ': ' + media_item.link.strip,
+		:image_url => media_item.images.standard_resolution.url,
+		:title => media_item.link,
+		:thumb_url => media_item.images.thumbnail.url,
+		:title_link => media_item.link,
+		:text => caption,
 	}
 
-	HTTParty.post(webhook_url, body: { payload: payload.to_json })
-
-	# puts payload
+	attachments.push(attachment)
 
 	min_timestamp = created_time.to_i + 1
 end
 
+if attachments.length == 0
+	exit
+end
+
+payload = {
+	:channel => '#news',
+	:text => 'New Instagram ' + (attachments.length == 1 ? 'post' : 'posts') + ' from *@' + client.user(user).username.to_s + '*',
+	:username => 'ig beta',
+	:attachments => attachments,
+}
+
+# puts payload
+
+HTTParty.post(webhook_url, body: { payload: payload.to_json })
 f = File.new(min_timestamp_file, 'w')
 f.write(min_timestamp)
 f.close
